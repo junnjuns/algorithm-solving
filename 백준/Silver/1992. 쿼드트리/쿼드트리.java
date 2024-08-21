a2b641a2c4848d20734ccff75245a657e69f8bcf
junnjuns <94726711+junnjuns@users.noreply.github.com>
2024-08-21 23:11:51 +0900

    [Silver I] Title: 쿼드트리, Time: 104 ms, Memory: 15492 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,6 +1,18 @@
 import java.util.*;
 import java.io.*;
 
+
+//board 를 입력 받는다.
+//압축 로직 실행 : divide(초기 위치 좌표, 초기 압축 할 보드의 사이즈)
+    //만약 board를 모두 확인했을 때 압축 가능한지 확인
+        //압축 가능할 때
+            //해당 board의 값 1개 출력
+            //
+        //압축 불가능 할 떄
+            //괄호 열기
+            // 4등분 재귀 로직 실행
+            //괄호 닫기
+
 public class Main {
 
     static int boardSize;
@@ -46,7 +58,6 @@ public class Main {
 		
 		if(check){ //해당 지역을 모두 확인했을 때 압축을 할 수 있다면 해당 지역 번호를 출력후 return;
 		    bw.write(first+"");
-		    return;
 		}
 		else{ //해당 지역을 모두 확인했을 때 압축을 하지 못한다면 4등분 재귀
 		    bw.write("(");
