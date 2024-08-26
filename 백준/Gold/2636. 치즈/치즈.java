4c1d4540405c3839b096a4a40f9ed626ecfec02d
junnjuns <94726711+junnjuns@users.noreply.github.com>
2024-08-26 21:19:46 +0900

    [Gold IV] Title: 치즈, Time: 156 ms, Memory: 16740 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,75 +1,84 @@
 import java.util.*;
 import java.io.*;
 
-public class Main
-{
-    static int n,m;
-    static int[][] board;
-    static int[][] vis;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
-    static int cheese;
-    
+public class Main {
+	
+	static int[][] board;
+	static int row, col;
+	static int[] dx = {1,-1,0,0};
+	static int[] dy = {0,0,1,-1};
+	static int[][] vis;
+	
 	public static void main(String[] args) throws Exception {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	    StringTokenizer st;
-	    
-	    st = new StringTokenizer(br.readLine());
-	    n = Integer.parseInt(st.nextToken());
-	    m = Integer.parseInt(st.nextToken());
-	    
-	    board = new int[n][m];
-	    
-	    for(int i=0; i<n; i++){
-	        st = new StringTokenizer(br.readLine());
-	        for(int j=0; j<m; j++){
-	            board[i][j] = Integer.parseInt(st.nextToken());
-	            if(board[i][j] == 1){
-	                cheese++;
-	            }
-	        }
-	    }
-	    
-	    int time = 0;
-	    int res = 0;
-	    while(cheese != 0){
-	        res = cheese;
-	        time++;
-	        vis = new int[n][m];
-	        bfs(0,0);
-	    }
-	    bw.write(time+"\n");
-	    bw.write(res+"");
-	    bw.flush();
-	    bw.close();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		row = Integer.parseInt(st.nextToken());
+		col = Integer.parseInt(st.nextToken());
+		
+		board = new int[row][col];
+		
+		for(int h = 0; h < row; h++){
+		    st = new StringTokenizer(br.readLine());
+		    for(int w = 0; w < col; w++){
+		        board[h][w] = Integer.parseInt(st.nextToken());
+		    }
+		}
+		
+		int time = 0;
+		ArrayList<Integer> list = new ArrayList<>();
+		while(true){
+		    
+		    boolean check = true;
+		    vis = new int[row][col];
+		    int cnt = 0;
+		    for(int h = 0; h < row; h++){
+    		    for(int w = 0; w < col; w++){
+    		        if(board[h][w] == 1){
+    		            check = false;
+    		            cnt += 1;
+    		        }
+    		    }
+		    }
+		    list.add(cnt);
+		    if(check) break;
+		    
+		    time += 1;
+		    fill(0, 0);
+		}
+		
+		bw.write(time+"\n");
+		bw.write(list.get(list.size() - 2)+"");
+		bw.flush();
+		bw.close();
 	}
 	
-	static void bfs(int i, int j){
-	    ArrayDeque<int[]> dq = new ArrayDeque<>();
-	    dq.add(new int[] {i,j});
-	    vis[i][j] = 1;
-	    
-	    while(dq.size() != 0){
-	        int[] now = dq.poll();
-	        
-	        for(int k=0; k<4; k++){
-	            int x = now[0] + dx[k];
-	            int y = now[1] + dy[k];
-	            
-	            if(x>=0 && y>=0 && x<n && y<m){
-	                if(board[x][y] == 0 && vis[x][y] == 0){
-	                    dq.add(new int[] {x,y});
-	                    vis[x][y] = 1;
-	                }
-	                else if (board[x][y] == 1 && vis[x][y] == 0){
-	                    board[x][y] = 0;
-	                    vis[x][y] = 1;
-	                    cheese -= 1;
-	                }
-	            }
-	        }
-	        
-	    }
+	static void fill(int sx, int sy){
+        ArrayDeque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[] {sx, sy});
+        vis[sx][sy] = 1;
+        
+        while(dq.size() != 0){
+            int[] now = dq.poll();
+            
+            for(int dir = 0; dir < 4; dir++){
+                int nx = now[0] + dx[dir];
+                int ny = now[1] + dy[dir];
+                
+                if(nx >= 0 && ny >= 0 && nx < row && ny < col){
+                    if(vis[nx][ny] == 0 && board[nx][ny] == 0){
+                        dq.add(new int[] {nx, ny});
+                        vis[nx][ny] = 1;
+                    }
+                    else if(board[nx][ny] == 1){
+                        board[nx][ny] = 0;
+                        vis[nx][ny] = 1;
+                    }
+                }
+            }
+            
+        }
 	}
 }
