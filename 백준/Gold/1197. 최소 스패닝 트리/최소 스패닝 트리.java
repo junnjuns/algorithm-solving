f70bae7d501e999bc6ea1e580dff1dd125a780d3
junnjuns <94726711+junnjuns@users.noreply.github.com>
2024-09-02 00:19:36 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 516 ms, Memory: 49484 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static class Edge implements Comparable<Edge>{
+
+        int start;
+        int end;
+        int cost;
+
+        public Edge(int start, int end, int cost){
+            this.start = start;
+            this.end = end;
+            this.cost = cost;
+        }
+
+        public int compareTo(Edge o){
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+
+    static int nodeCnt;
+    static int edgeCnt;
+    static Edge[] edges;
+    static int[] parents;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        nodeCnt = Integer.parseInt(st.nextToken());
+        edgeCnt = Integer.parseInt(st.nextToken());
+
+        edges = new Edge[edgeCnt];
+
+        for(int idx = 0; idx < edgeCnt; idx++){
+            st = new StringTokenizer(br.readLine());
+            int nodeA = Integer.parseInt(st.nextToken()) - 1;
+            int nodeB = Integer.parseInt(st.nextToken()) - 1;
+            int cost = Integer.parseInt(st.nextToken());
+
+            edges[idx] = new Edge(nodeA, nodeB, cost);
+        }
+
+        Arrays.sort(edges);
+
+        parents = new int[nodeCnt];
+        for(int idx = 0; idx < nodeCnt; idx++){
+            parents[idx] = idx;
+        }
+
+        long answer = 0;
+        for(Edge edge : edges){
+            if(union(edge.start, edge.end)){
+                answer += edge.cost;
+            }
+        }
+
+        bw.write(answer+"");
+        bw.flush();
+        bw.close();
+    }
+
+    static boolean union(int nodeA, int nodeB){
+        int aRoot = find(nodeA);
+        int bRoot = find(nodeB);
+
+        if(aRoot == bRoot){
+            return false;
+        }
+
+        if(aRoot > bRoot){
+            parents[aRoot] = bRoot;
+        }
+        else{
+            parents[bRoot] = aRoot;
+        }
+
+        return true;
+    }
+
+    static int find(int node){
+        if(parents[node] == node){
+            return node;
+        }
+        return parents[node] = find(parents[node]);
+    }
+}
