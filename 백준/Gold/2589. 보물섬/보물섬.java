412dea51881a10d984af35a22a43219d90b0ae37
junnjuns <94726711+junnjuns@users.noreply.github.com>
2025-03-11 23:33:08 +0900

    [Gold V] Title: 보물섬, Time: 448 ms, Memory: 187928 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -1,67 +1,77 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main
 {   
-    static int n;
-    static int m;
+    static int row, col;
     static char[][] board;
     static int[][] vis;
-    static int[] dx ={1,-1,0,0};
-    static int[] dy ={0,0,1,-1};
-    static int ans;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    
 	public static void main(String[] args) throws Exception {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    StringTokenizer st;
 	    
-	    StringTokenizer st = new StringTokenizer(br.readLine());
-	    n = Integer.parseInt(st.nextToken());
-	    m = Integer.parseInt(st.nextToken());
-	    board = new char[n][m];
+	    st = new StringTokenizer(br.readLine());
 	    
-	    for(int i=0; i<n; i++){ //입력값 배열 넣기
-	        String s = br.readLine();
-	        for(int j=0; j<m; j++){
-	            board[i][j] = s.charAt(j);
+	    row = Integer.parseInt(st.nextToken());
+	    col = Integer.parseInt(st.nextToken());
+	    
+	    board = new char[row][col];
+	    
+	    
+	    for(int h = 0; h < row; h++){
+	        String str = br.readLine();
+	        for(int w = 0; w < col; w++){
+	            board[h][w] = str.charAt(w);
+	        }
+	    } //입력 끝
+	    
+	    int answer = Integer.MIN_VALUE;
+	    for(int h = 0; h < row; h++){
+	        for(int w = 0; w < col; w++){
+	            //육지일때만 탐색 ㄱㄱ
+	            if(board[h][w] == 'L'){
+	                vis = new int[row][col];
+	                answer = Math.max(answer, bfs(h, w));
+	            }            
 	        }
 	    }
-	    
-	    for(int i=0; i<n; i++){
-	        for(int j=0; j<m; j++){
-	            if(board[i][j] == 'L'){
-                    vis = new int[n][m];
-	                BFS(i,j);
-	            }
-	        }
-	    }
-	    bw.write(ans+"");
+	    bw.write(answer+"");
 	    bw.flush();
 	    bw.close();
 	}
-	
-	static void BFS(int i, int j){
-	    
+	static int bfs(int sx, int sy){
 	    ArrayDeque<int[]> dq = new ArrayDeque<>();
-	    dq.offer(new int[] {i,j});
-	    vis[i][j] = 1;
+	    dq.add(new int[] {sx, sy});
+	    vis[sx][sy] = 1;
 	    
+        int num = 0;	    
 	    while(dq.size() != 0){
 	        int[] now = dq.poll();
-	        
-	        for(int k=0; k<4; k++){
-	            int x = now[0] + dx[k];
-	            int y = now[1] + dy[k];
+	        num = vis[now[0]][now[1]];
+	        for(int dir = 0; dir < 4; dir++){
+	            int nx = now[0] + dx[dir];
+	            int ny = now[1] + dy[dir];
 	            
-	            if(x>=0 && y>=0 && x<n && y<m){
-	                if(vis[x][y] == 0 && board[x][y] == 'L'){
-	                    dq.add(new int[] {x,y});
-	                    vis[x][y] = vis[now[0]][now[1]]+1;
-	                    ans = Math.max(ans, vis[x][y]-1);
+	            //만약 범위 안 + 땅일 때
+	            if(check(nx, ny) && board[nx][ny] =='L'){
+	                //방문 안했을 때
+	                if(vis[nx][ny] == 0){
+	                    vis[nx][ny] = vis[now[0]][now[1]] + 1;
+	                    dq.add(new int[] {nx, ny});
 	                }
 	            }
 	        }
+	        
 	    }
+	    return num - 1;
 	}
 	
-	
+	static boolean check(int x, int y){
+	    return x >= 0 && y >= 0 && x < row && y < col;
+	}
 }
