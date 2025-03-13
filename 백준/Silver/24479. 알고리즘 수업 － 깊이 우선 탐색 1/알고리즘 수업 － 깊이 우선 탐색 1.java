f0d878bc81f4b0a638cb00cf3ebd47042b5d984d
junnjuns <94726711+junnjuns@users.noreply.github.com>
2025-03-13 19:08:49 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 896 ms, Memory: 100008 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -1,62 +1,74 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    static int n, m, r;
-    static boolean[] vis;
+public class Main
+{
+    static int nodeCnt;
+    static int edgeCnt;
+    static int startNode;
     static List<Integer>[] list;
-    static int[] arr;
-    static BufferedWriter bw;
+    static boolean[] vis;
+    static int[] answer;
     static int cnt = 1;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-
-        list = new ArrayList[n + 1];
-        vis = new boolean[n + 1];
-        arr = new int[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            list[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            list[a].add(b);
-            list[b].add(a);
-        }
-
-        for (int i = 1; i <= n; i++) {
-            Collections.sort(list[i]);
-        }
-
-        DFS(r);
-
-        for (int i = 1; i <= n; i++) {
-            bw.write(arr[i] + "\n");
-        }
-        bw.flush();
-        bw.close();
-    }
-
-    static void DFS(int node) {
-        vis[node] = true;
-        arr[node] = cnt++;
-
-        for (int next : list[node]) {
-            if (!vis[next]) {
-                DFS(next);
-            }
-        }
-    }
+    
+	public static void main(String[] args) throws Exception{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    StringTokenizer st;
+	    
+	    st = new StringTokenizer(br.readLine());
+	    
+	    nodeCnt = Integer.parseInt(st.nextToken());
+	    edgeCnt = Integer.parseInt(st.nextToken());
+	    startNode = Integer.parseInt(st.nextToken());
+	    
+	    answer = new int[nodeCnt + 1];
+	    vis = new boolean[nodeCnt + 1];
+	    
+	    list = new ArrayList[nodeCnt + 1];
+	    
+	    for(int idx = 0; idx < nodeCnt + 1; idx++){
+	        list[idx] = new ArrayList<>();
+	    }
+	    
+	    for(int idx = 0; idx < edgeCnt; idx++){
+	        st = new StringTokenizer(br.readLine());
+	        int nodeA = Integer.parseInt(st.nextToken());
+	        int nodeB = Integer.parseInt(st.nextToken());
+	        
+	        list[nodeA].add(nodeB);
+	        list[nodeB].add(nodeA);
+	    }
+	   
+	    
+	    for(int idx = 1; idx < nodeCnt + 1; idx++){
+	       
+	       if(list[idx].size() > 1){
+	           Collections.sort(list[idx]);
+	       }
+            	       
+	    }
+	    
+	    dfs(startNode);
+	    
+	    
+	    for(int idx = 1; idx < nodeCnt + 1; idx++){
+	        bw.write(answer[idx]+"\n");	       
+	    }	    
+	    
+	    bw.flush();
+	    bw.close();
+	}
+	
+	static void dfs(int start){
+	    vis[start] = true;
+	    answer[start] = cnt;
+	    cnt += 1;
+	    
+	    for(int idx = 0; idx < list[start].size(); idx++){
+	        if(vis[list[start].get(idx)] == false){
+	            dfs(list[start].get(idx));
+	        }
+	    }
+	}
 }
