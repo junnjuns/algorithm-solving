fea9f555b56fcb5b2563b045bc4c6c46b3ed9308
junnjuns <94726711+junnjuns@users.noreply.github.com>
2025-03-14 10:03:45 +0900

    [Gold V] Title: 숫자고르기, Time: 68 ms, Memory: 11648 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{   
+    static int n;
+    static int[] arr;
+    static int[] vis;
+    static ArrayList<Integer> list;
+    static int answer;
+    
+	public static void main(String[] args) throws Exception {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    
+	    n = Integer.parseInt(br.readLine());
+	    
+	    arr = new int[n + 1];
+	    
+	    for(int idx = 1; idx < n + 1; idx++){
+	        arr[idx] = Integer.parseInt(br.readLine());
+	    }
+        
+        vis = new int[n + 1];
+        
+        list = new ArrayList<>();
+        
+        for(int idx = 1; idx < n + 1; idx++){
+            vis[idx] = 1;
+            dfs(idx, idx);
+            vis[idx] = 0;
+            
+        }        
+	    
+	    bw.write(answer+"");
+	    
+	    bw.newLine();
+	    
+	    Collections.sort(list);
+	    for(int idx = 0; idx < list.size(); idx++){
+	        bw.write(list.get(idx)+"\n");
+	    }
+	    
+	    bw.flush();
+	    bw.close();
+	}
+	
+	static void dfs(int start , int pre){
+	    
+	    if(arr[start] == pre){
+	        list.add(pre);
+	        answer += 1;
+	    }
+	    
+	    if(vis[arr[start]] == 0){
+            vis[arr[start]] = 1;
+            dfs(arr[start], pre);
+            vis[arr[start]] = 0;
+	    }
+	    
+	}
+}
+
+
+
+
+
+
+
