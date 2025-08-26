908d75d4c92d1576214db7b5f3f634b7e3e7f94a
junnjuns <94726711+junnjuns@users.noreply.github.com>
2025-08-26 20:58:42 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 120 ms, Memory: 13736 KB -BaekjoonHub

--- 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.java
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.java
@@ -7,7 +7,7 @@ public class Main
 {
     static int n;
     static int[][] board;
-    static ArrayList<Integer>[] list;
+    static int[] cnt;
     
 	public static void main(String[] args) throws Exception {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,24 +25,22 @@ public class Main
             }
         } //입력 끝
         
-        list = new ArrayList[n];
-        for(int idx = 0; idx < n; idx++){
-            list[idx] = new ArrayList<>();
-        }
         
+        cnt = new int[n];
         
+        //학생
         for(int student = 0; student < n - 1; student++){
             
-            for(int grade = 0; grade < 5; grade++){
-                for(int other = student + 1; other < n; other++){
+            //다른 학생
+            for(int other = student + 1; other < n; other++){
+                //학년
+                for(int grade = 0; grade < 5; grade++){
                     
                     //같은반 일 때
                     if(board[student][grade] == board[other][grade]){
-                        //중복 제거
-                        if(!list[student].contains(other)){
-                            list[student].add(other);
-                            list[other].add(student);
-                        }
+                        cnt[student] += 1;
+                        cnt[other] += 1;
+                        break;
                     }
                     
                     
@@ -50,9 +48,9 @@ public class Main
             }
         }
         int answer = 0;
-        for(int student = 0; student < n; student++){
+        for(int student = 1; student < n; student++){
             //같은 값 여러개일 때,가장 작은 번호만 출력이기에 초과일때만 조건 설정
-            if(list[student].size() > list[answer].size()){
+            if(cnt[student] > cnt[answer]){
                 //임시반장 초기화
                 answer = student;
             }
