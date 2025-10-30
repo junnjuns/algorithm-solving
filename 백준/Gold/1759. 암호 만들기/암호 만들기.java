39795fc61c0c30d0a44db6c7fceaffe8c42ca50c
junnjuns <94726711+junnjuns@users.noreply.github.com>
2025-10-30 16:28:37 +0900

    [Gold V] Title: 암호 만들기, Time: 68 ms, Memory: 12108 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,79 +1,74 @@
+//  최소 한 개의 모음(a, e, i, o, u)과 최소 두 개의 자음으로 구성
+
+
 import java.util.*;
 import java.io.*;
 
-
-
-
-public class Main {
+public class Main
+{
     
-    static int len;
-    static int count;
-    static char[] alphabet;
-    static BufferedWriter bw;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    static int n, m;
+    static char[] elementArr;
     static char[] selectArr;
-    static boolean[] vis;
-    
-    
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
-        
-        len = Integer.parseInt(st.nextToken());
-        count = Integer.parseInt(st.nextToken());
-        
-        vis = new boolean[count];
-        alphabet = new char[count];
-        selectArr = new char[len];
-        
-        st = new StringTokenizer(br.readLine());
-        for(int idx = 0; idx < count; idx++){
-            alphabet[idx] = st.nextToken().charAt(0);
-        }
-        
-        Arrays.sort(alphabet);
-        permut(0, 0);
-        
-        bw.close();
+    static char[] vowel = {'a', 'e', 'i', 'o', 'u'};
+	public static void main(String[] args) throws Exception {
+	    
+	    StringTokenizer st;
+	    
+	    st = new StringTokenizer(br.readLine());
+	    
+	    n = Integer.parseInt(st.nextToken());
+	    m = Integer.parseInt(st.nextToken());
+	    
+	    elementArr = new char[m];
+	    selectArr = new char[n];
+	    
+	    st = new StringTokenizer(br.readLine());
+	    for(int idx = 0; idx < m; idx++){
+	        elementArr[idx] = st.nextToken().charAt(0);
+	    }
+	    
+	    Arrays.sort(elementArr);
+	    
+	    func(0, 0);
+	    
+	    bw.flush();
+	    bw.close();
+	    
     }
     
-    static void permut(int select,int start) throws Exception {
-        if(select == len){
-            
-            boolean check = false;
+    static void func(int depth, int start) throws Exception {
+        if(depth == n){
             int cnt = 0;
-            for(char c : selectArr){
-                if(c == 'a' || c == 'e' ||c == 'i' ||c == 'o' ||c == 'u'){
-                    check = true;
-                    cnt++;
+            
+            for(char ch : selectArr){
+                for(char vw : vowel){
+                    if(ch == vw){
+                        cnt += 1;
+                    }
                 }
             }
             
-            if(cnt > len - 2){
-                return;
-            }
-            
-            if(check){
-                for(char c : selectArr){
-                    bw.write(c+"");    
+            if(cnt > 0 && cnt <= n - 2 ){
+                for(char ch : selectArr){
+                    bw.write(ch+"");
                 }
                 bw.newLine();
             }
+            
             return;
         }
         
-        for(int idx = start; idx < count; idx++){
-            if(vis[idx]) continue;
+        for(int idx = start; idx < m; idx++){
             
-            
-            vis[idx] = true;
-            selectArr[select] = alphabet[idx];
-            permut(select + 1, idx);
-            vis[idx] = false;
-            
+            selectArr[depth] = elementArr[idx];
+            func(depth + 1, idx + 1);
         }
+        
+        
         
     }
     
