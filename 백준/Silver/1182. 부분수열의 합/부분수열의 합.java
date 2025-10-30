967b09d398cfc85307f08e716b5d033fc8ed90a5
junnjuns <94726711+junnjuns@users.noreply.github.com>
2025-10-30 14:00:25 +0900

    [Silver II] Title: 부분수열의 합, Time: 72 ms, Memory: 11632 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,53 +1,56 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main
-{   
-    static int n,s,cnt;
-    static int[] arr;
-    static int[] ans;
+{
+    static int n, res;
+    static int[] elementArr;
     static boolean[] vis;
+    static int answer;
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		s = Integer.parseInt(st.nextToken());
-		
-		arr = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++){
-		    arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		DFS(0, 0);
-		
-		if(s == 0){
-		    bw.write(cnt-1+"");
-		}
-		else{
-		    bw.write(cnt+"");
-		}
-		
-		bw.flush();
-		bw.close();
-		
-	}
-	
-	static void DFS(int dep, int sum){
-	    if(dep == n){
-	        if(sum == s){
-	            cnt++;
-	        }
-	        return;
+	    
+	    StringTokenizer st;
+	    st = new StringTokenizer(br.readLine());
+	    n = Integer.parseInt(st.nextToken());
+	    res = Integer.parseInt(st.nextToken());
+	    
+	    elementArr = new int[n];
+	    vis = new boolean[n];
+	    
+	    st = new StringTokenizer(br.readLine());
+	    
+	    for(int idx = 0; idx < n; idx++){
+	        elementArr[idx] = Integer.parseInt(st.nextToken());
 	    }
 	    
-	    DFS(dep+1, sum);
-	    DFS(dep+1, sum + arr[dep]);
-	}
+	    subset(0, 0);
+	    
+	    if(res == 0){
+	        answer -= 1;
+	    }
+	    
+	    bw.write(answer+"");
+	    bw.flush();
+	    bw.close();
+	    
+    }
+
+    static void subset(int depth, int sum){
+        if(depth == n){
+            if(sum == res){
+                answer += 1;
+            }
+            return;
+        }
+        
+        subset(depth + 1, sum + elementArr[depth]);
+        
+        subset(depth + 1, sum);
+        
+    }
+    
 }
