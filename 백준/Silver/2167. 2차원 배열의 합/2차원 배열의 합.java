435b8318c2c9c2bfe744a9c97d0bbfdf1ae36397
junnjuns <94726711+junnjuns@users.noreply.github.com>
2025-10-30 19:50:08 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 2400 ms, Memory: 52000 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -1,67 +1,57 @@
+
+
 import java.util.*;
 import java.io.*;
 
-
 public class Main
 {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    static int n, m;
+    static int[][] board;
+    static int k;
+    
 	public static void main(String[] args) throws Exception {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    
 	    StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        board = new int[n][m];
+        
+        for(int row = 0; row < n; row++){
+            st = new StringTokenizer(br.readLine());
+            for(int col = 0; col < m; col++){
+                board[row][col] = Integer.parseInt(st.nextToken());
+            }
+        }
 	    
-	    st = new StringTokenizer(br.readLine());
-	    int n = Integer.parseInt(st.nextToken());
-	    int m = Integer.parseInt(st.nextToken());
+	    k = Integer.parseInt(br.readLine());
 	    
-	    int[][] arr = new int[n][m];
-	    
-	    for(int h = 0; h < n; h++){
+	    for(int idx = 0; idx < k; idx++){
 	        st = new StringTokenizer(br.readLine());
-	        for(int w = 0; w < m; w++){
-	            arr[h][w] = Integer.parseInt(st.nextToken());
-	        }
-	    }
-	    
-
-	    int k = Integer.parseInt(br.readLine());
-	    
-	    for(int i = 0; i < k; i++){
-	        st = new StringTokenizer(br.readLine());
-	        int s1 = Integer.parseInt(st.nextToken())-1;
-	        int s2 = Integer.parseInt(st.nextToken())-1;
-	        
-	        int e1 = Integer.parseInt(st.nextToken())-1;
-	        int e2 = Integer.parseInt(st.nextToken())-1;
+	        int x1 = Integer.parseInt(st.nextToken()) - 1;
+	        int y1 = Integer.parseInt(st.nextToken()) - 1;
+	        int x2 = Integer.parseInt(st.nextToken()) - 1;
+	        int y2 = Integer.parseInt(st.nextToken()) - 1;
 	        
 	        int sum = 0;
-	        if(s1 == e1){
-	            for(int j = s2; j <= e2; j++){
-	                sum += arr[s1][j];
+	        for(int row = x1; row <= x2; row++){
+	            for(int col = y1; col <= y2; col++){
+	                sum += board[row][col];
 	            }
 	        }
-	        else{
-	                
-	           for(int q = s2; q <= e2; q++){
-	               sum += arr[s1][q];
-	           }
-	           
-	           for(int q = s1+1; q < e1; q++){
-	               
-	               for(int p = s2; p <= e2; p++){
-	                    sum += arr[q][p];    
-	               }
-	               
-	           }
-	           
-	           for(int q = s2; q <= e2; q++){
-	               sum += arr[e1][q];
-	           }
-	        }
-	        
 	        bw.write(sum+"\n");
 	    }
 	    
+	    
 	    bw.flush();
 	    bw.close();
-	}
+	    
+    }
+    
 }
