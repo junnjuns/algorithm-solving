28c53476d312a3d9d415d0edc3adc0dc767b5412
junnjuns <94726711+junnjuns@users.noreply.github.com>
2025-10-30 13:40:15 +0900

    [Silver III] Title: N과 M (1), Time: 1576 ms, Memory: 62068 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,46 +1,61 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-
-	
-	static int n,m;
-	static int[] ans;
-	static BufferedWriter bw;
-	static boolean[] vis;
+public class Main
+{
+    static int n, m;
+    static int[] selectArr;
+    static int[] elementArr;
+    static boolean[] vis;
+    
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		ans = new int[m];
-		vis = new boolean[n+1];
-		DFS(0);
-		
-		bw.flush();
-		bw.close();
-	}
-	
-	static void DFS(int dep) throws Exception{
-		if(dep == m) {
-			for(int i: ans) {
-				bw.write(i+" ");
-			}
-			bw.newLine();
-			return;
-		}
-		
-		for(int i=1; i<=n; i++) {
-			if(vis[i] == false) {
-				vis[i] = true;
-				ans[dep] = i;
-				DFS(dep+1);
-				vis[i] = false;
-			}
-		}
-	}
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    StringTokenizer st;
+	    
+	    st = new StringTokenizer(br.readLine());
+	    
+	    n = Integer.parseInt(st.nextToken());
+	    m = Integer.parseInt(st.nextToken());
+	    
+	    elementArr = new int[n];
+	    selectArr = new int[m];
+	    
+	    vis = new boolean[n];
+	    
+	    for(int idx = 0; idx < n; idx++){
+	        elementArr[idx] = idx + 1;
+	    }
+	    
+	    permut(0);
+	    
+	    
+	    bw.flush();
+	    bw.close();
+	    
+    }
+    
+    static void permut(int depth){
+        if(depth == m){
+            for(int value : selectArr){
+                System.out.print(value+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int idx = 0; idx < n; idx++){
+            if(vis[idx]) continue;
+            
+            vis[idx] = true;
+            selectArr[depth] = elementArr[idx];
+            permut(depth + 1);
+            vis[idx] = false;
+            
+        }
+
+    }
+    
+
+    
 }
