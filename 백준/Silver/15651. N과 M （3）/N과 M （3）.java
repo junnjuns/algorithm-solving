ea55efbf834b1a6f662dac008e8f9697420ef915
junnjuns <94726711+junnjuns@users.noreply.github.com>
2025-10-30 14:43:17 +0900

    [Silver III] Title: N과 M (3), Time: 504 ms, Memory: 164228 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,42 +1,58 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-
-	
-	static int n,m;
-	static int[] ans;
-	static BufferedWriter bw;
-	static boolean[] vis;
+public class Main
+{
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    static int n;
+    static int m;
+    static int[] elementArr;
+    static int[] selectArr;
+    
+    
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		ans = new int[m];
-		vis = new boolean[n+1];
-		DFS(0);
-		
-		bw.flush();
-		bw.close();
-	}
-	
-	static void DFS(int dep) throws Exception{
-		if(dep == m) {
-			for(int i: ans) {
-				bw.write(i+" ");
-			}
-			bw.newLine();
-			return;
-		}
-		
-		for(int i=1; i<=n; i++) {
-				ans[dep] = i;
-				DFS(dep+1);
-		}
-	}
+	    
+	    StringTokenizer st;
+	    st = new StringTokenizer(br.readLine());
+	    
+	    n = Integer.parseInt(st.nextToken());
+	    m = Integer.parseInt(st.nextToken());
+	    
+	    elementArr = new int[n];
+	    selectArr = new int[m];
+	    
+	    for(int idx = 0; idx < n; idx++){
+	        elementArr[idx] = idx + 1;
+	    }
+	    
+        func(0);
+        
+        
+	    
+	    bw.flush();
+	    bw.close();
+	    
+    }
+    
+    static void func(int depth) throws Exception {
+        if(depth == m){
+            for(int value : selectArr){
+                bw.write(value+" ");
+            }
+            bw.newLine();
+            return;
+        }
+        
+        for(int idx = 0; idx < n; idx++){
+            
+            selectArr[depth] = elementArr[idx];
+            func(depth + 1);
+            
+        }
+    
+    }
+    
 }
