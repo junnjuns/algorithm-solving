fcdb418ac366f88ce737fe4c17bdc62664a1aeed
junnjuns <94726711+junnjuns@users.noreply.github.com>
2025-12-05 10:39:01 +0900

    [level 1] Title: 콜라 문제, Time: 0.35 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,24 +1,18 @@
 // 빈 병 a개를 가져다 주면 b개의 콜라를 준다.
 // n개의 빈병이 있다.
 class Solution {
-    static int answer;
     
     public int solution(int a, int b, int n) {
-        
-        
-        func(a, b, n);
-        
-        return answer;
-    }
     
-    static void func(int a, int b, int n){
         if(n < a){
-            return;
+            return 0;
         }
         
-        answer += (n / a) * b;
-        n = (n / a) * b + (n % a);
-        func(a, b, n);
+        int get  = (n / a) * b;
+        int next = get + (n % a);
         
+        
+        return get + solution(a, b, next);
     }
+    
 }
