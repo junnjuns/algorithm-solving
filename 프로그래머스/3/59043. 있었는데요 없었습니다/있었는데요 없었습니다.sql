50349bb1ca505d71034c3b89fc1f061bd7082d71
junnjuns <94726711+junnjuns@users.noreply.github.com>
2026-03-21 16:04:58 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,8 +1,8 @@
 select
-i.ANIMAL_ID,
-i.NAME
-from
-ANIMAL_INS as i join ANIMAL_OUTS as o
-on i.ANIMAL_ID = o.ANIMAL_ID
-where i.DATETIME > o.DATETIME
-order by i.DATETIME
+ai.ANIMAL_ID,
+ai.NAME
+from ANIMAL_INS as ai
+join ANIMAL_OUTS as ao
+    on ai.ANIMAL_ID = ao.ANIMAL_ID
+where ai.DATETIME > ao.DATETIME
+order by ai.DATETIME;
