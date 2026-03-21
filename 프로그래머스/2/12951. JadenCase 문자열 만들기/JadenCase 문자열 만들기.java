901047afa58b3325c718266916ad5c12a2d629dc
junnjuns <94726711+junnjuns@users.noreply.github.com>
2026-03-21 17:34:34 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.13 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,39 +1,25 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        StringBuffer sb = new StringBuffer();
-        
-        boolean isEmpty = true; //빈칸이면 true
-        
-        for(int i = 0; i < s.length(); i++){
+        StringBuilder sb = new StringBuilder();
+        boolean isStartOfWord = true;
+
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            
-            if(ch == ' '){
+
+            if (ch == ' ') {
                 sb.append(ch);
-                isEmpty = true;
+                isStartOfWord = true;
                 continue;
             }
-            
-            if(isEmpty && ch != ' '){ //만약 젤 앞일 때
-                if(Character.isLowerCase(ch)){
-                    ch = Character.toUpperCase(ch);
-                }
-                sb.append(ch);
-                isEmpty = false;
+
+            if (isStartOfWord) {
+                sb.append(Character.toUpperCase(ch));
+                isStartOfWord = false;
+            } else {
+                sb.append(Character.toLowerCase(ch));
             }
-            else if(!isEmpty && ch != ' '){
-                if(Character.isUpperCase(ch)){
-                    ch = Character.toLowerCase(ch);
-                }
-                sb.append(ch);
-            }
-            
-        }// for문 끝
-        
-        
+        }
+
         return sb.toString();
     }
 }
