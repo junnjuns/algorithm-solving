ccee340f1d3e5e26e3fdd67fa741f529f04fefc3
junnjuns <94726711+junnjuns@users.noreply.github.com>
2026-03-21 15:06:51 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,9 +1,9 @@
 select
-i.NAME,
-i.DATETIME
-from
-ANIMAL_INS as i left join ANIMAL_OUTS as o
-on i.ANIMAL_ID = o.ANIMAL_ID
-where o.DATETIME is null
-order by i.DATETIME
+ai.NAME,
+ai.DATETIME
+from ANIMAL_INS as ai
+    left join ANIMAL_OUTS as ao
+    on ai.ANIMAL_ID = ao.ANIMAL_ID
+where ao.ANIMAL_ID is null
+order by ai.DATETIME
 limit 3
