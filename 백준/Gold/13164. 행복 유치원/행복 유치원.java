76e8b7b6e271d0ea5c745832d947992c3e60ef17
junnjuns <94726711+junnjuns@users.noreply.github.com>
2026-04-20 19:06:27 +0900

    [Gold V] Title: 행복 유치원, Time: 460 ms, Memory: 63300 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main
+{   
+    
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int[] diff = new int[n - 1];
+        
+        for(int i = 0; i < n - 1; i++){
+            diff[i] = arr[i + 1] - arr[i];
+        }
+        Arrays.sort(diff);
+        
+        int answer = 0;
+        for(int i = 0; i < n - k; i++){
+            answer += diff[i];
+        }
+        
+        bw.write(answer+"");
+		bw.flush();
+		bw.close();
+	}
+	
+	
+}
